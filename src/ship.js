2b6aea7a3a1f21ef12e9ee25551cf9bb24fcc13d
Lippiece <lippiece@proton.me>
2022-10-08 14:07:18 +0200

    fix hit() not returning changed state

--- src/ship.js
+++ src/ship.js
@@ -11,9 +11,9 @@ const shipMethods = {
   hit:
     ship =>
       ( {
+        ...ship,
         hitCount: ship.hitCount + 1,
         isSunk  : ship.hitCount + 1 === ship.length,
-        ...ship,
       } ),
 };
 
